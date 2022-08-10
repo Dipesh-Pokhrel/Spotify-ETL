941f64917d94ad87981e0f7cbf13d773a9e18986
Dipesh Pokhrel <dipeshpokhrel45@gmail.com>
2022-08-10 22:29:57 +0545

    authentication added

--- tools/playlists.py
+++ tools/playlists.py
@@ -1,6 +1,12 @@
 import spotipy
 
-spotify = spotipy.Spotify(client_credentials_manager=spotipy.oauth2.SpotifyClientCredentials())
+import configparser
+config = configparser.ConfigParser()
+config.read_file(open('config\spotify.config'))
+cid = config.get('Spotify', 'cid')
+secret = config.get('Spotify', 'secret')
+
+spotify = spotipy.Spotify(client_credentials_manager=spotipy.oauth2.SpotifyClientCredentials(client_id=cid , client_secret=secret))
 
 def get_artists_from_playlist(playlist_uri):
     playlist_tracks = spotify.playlist_tracks(playlist_id=playlist_uri)
